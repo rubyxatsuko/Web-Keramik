4dc9d3c9c2ebc804aa2910203ec36855ac1094ed
rubyxatsuko <89366661+rubyxatsuko@users.noreply.github.com>
2024-10-02 22:56:15 +0700

    Add files via upload

---
+++ views/admin/views/edit.php
@@ -0,0 +1,379 @@
+<?php
+@include "../../../connection.php";
+
+// Mengaktifkan Session
+session_start();
+
+// Jika belum login tidak bisa masuk ke halaman ini
+
+?>
+
+
+<?php 
+// Menghubungkan ke database
+include '../../../connection.php';
+
+// mengambil id
+$id = $_GET['id'];
+
+// Menampilkan product di database
+$query = mysqli_query($conn,"SELECT * FROM tbl_product WHERE id_product = '$id'");
+
+if(mysqli_num_rows($query) > 0) {
+   $row       = mysqli_fetch_assoc($query);
+
+}
+
+// Membuat random string
+// agar nama file foto tidak sama
+function generateRandomString($length = 10)
+{
+   $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   $charactersLength = strlen($characters);
+   $randomString = '';
+   for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+   }
+   return $randomString;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Add Data Barang - WM Keramik</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="../Vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Link Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-warning sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">WM Keramik</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="../index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Control Panel
+            </div>
+
+            <!-- Nav Item - Charts -->
+
+            <li class="nav-item">
+                <a class="nav-link" href="admin.php">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Data Admin</span></a>
+            </li>
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Data Produk</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transaksi.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Data Transaksi</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                        <li class="nav-item dropdown no-arrow d-sm-none">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-search fa-fw"></i>
+                            </a>
+                            <!-- Dropdown - Messages -->
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                                <form class="form-inline mr-auto w-100 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-warning" type="button">
+                                                <i class="fas fa-search fa-sm"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </li>
+
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img class="img-profile rounded-circle" src="../img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="../profile.php">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="../php/logout.php" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <div class="container mt-3 my-3 mb-5">
+
+                    <div class="container">
+                        <div class="d-sm-flex align-items-center justify-content-between">
+                            <h1 class="h5 mb-0 text-gray-800">Page Admin - Edit Produk</h1>
+                        </div>
+                        <hr>
+
+                        <div class="row justify-content-center">
+                            <form action="" method="post" enctype="multipart/form-data">
+
+                                <div class="col-md-12 mb-4 mt-3">
+                                    <label for="name" class="form-label">Nama Produk</label>
+                                    <input type="text" name="name" class="form-control" id="name" value="<?= $row['name_product']; ?>">
+                                </div>
+                                <div class="col-md-12 mb-4 mt-4">
+                                    <label for="price" class="form-label">Harga Produk</label>
+                                    <input type="text" name="price" class="form-control" id="price" value="<?= $row['price_product']; ?>">
+                                </div>
+
+                                <div class="col-md-12 mb-4 mt-4">
+                                    <label for="image" class="form-label">Gambar produk saat ini</label>
+                                    <img class="mt-3 d-flex" src="../../assets/fotoProduk/<?= $row['image_product'] ?>" width="150">
+                                </div>
+
+                                <div class="col-md-12 mb-4 mt-4">
+                                    <label for="image" class="form-label">Ganti Gambar</label>
+                                    <input type="file" name="image" class="form-control" id="image">
+                                    <small class="text-danger"><strong>* upload gambar yang berekstensi png, jpg atau jpeg</strong></small>
+                                </div>
+
+                                <div class="col-md-12 mb-4 mt-4">
+                                    <label for="stok" class="form-label">Stok</label>
+                                    <input type="text" name="stok" class="form-control" id="stok" value="<?= $row['stok_product']; ?>">
+                                </div>
+
+                                <button type="submit" class="btn btn-primary" name="edit">Simpan Produk</button>
+                            </form>
+                            <?php
+                            // Membuat update product
+                            if (isset($_POST['edit'])) {
+                                $name         = htmlspecialchars($_POST['name']);
+                                $price        = htmlspecialchars($_POST['price']);
+                                $stok         = htmlspecialchars($_POST['stok']);
+
+                                // Membuat upload file foto
+                                $target_dir    = "../../../assets/fotoProduk/";
+                                $nama_file     = basename($_FILES["image"]["name"]);
+                                $target_file   = $target_dir . $nama_file;
+                                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                                $image_size    = $_FILES['image']['size'];
+                                $random_name   = generateRandomString(20);
+                                $new_name      = $random_name . "." . $imageFileType;
+
+                                // cek apakah nama dan harga kosong atau tidak
+                                // Kalau kosong tidak bisa diupload
+                                if ($name == '' || $price == '' || $stok == '' ) {
+                            ?>
+
+                                    <div class="alert alert-primary" role="alert">
+                                        Nama dan harga produk kopi wajib di isi!
+                                    </div>
+                                    <meta http-equiv="refresh" content="2;url=barang.php" />
+
+                                    <?php
+                                } else {
+                                    $query = mysqli_query($conn, "UPDATE tbl_product SET name_product = '$name', price_product = '$price', image_product = '$nama_file', stok_product = '$stok' WHERE id_product = '$id'");
+                                    if ($nama_file != '') {
+                                        if ($image_size > 50000000) { 
+                                    ?>
+
+                                            <div class="alert alert-primary mt-5" role="alert">
+                                                Ukuran gambar tidak boleh lebih dari 2 MB
+                                            </div>
+                                            <meta http-equiv="refresh" content="2;url=barang.php" />
+
+                                            <?php
+                                        } else {
+                                            // Jika foto tidak sesuai dengan ekstensi dibawah ini
+                                            // Maka akan muncul pesan peringatan melaui alert
+                                            if ($imageFileType != 'jpg' && $imageFileType != 'png' && $imageFileType != 'jpeg') {
+                                            ?>
+
+                                                <div class="alert alert-primary mt-5" role="alert">
+                                                    Hanya file jpg, jpeg dan png yang diperbolehkan
+                                                </div>
+                                                <meta http-equiv="refresh" content="2;url=barang.php" />
+
+                                                <?php
+                                            } else {
+                                                // upload file foto
+                                                move_uploaded_file($_FILES["image"]["tmp_name"], $target_dir . $nama_file);
+
+                                                $query = mysqli_query($conn, "UPDATE tbl_product SET image_product ='$nama_file' WHERE id_product = '$id'");
+                                                var_dump($query);
+                                                if ($query) {
+                                                ?>
+
+                                                    <div class="alert alert-success mt-5" role="alert">
+                                                        Produk kopi berhasil di update!
+                                                    </div>
+                                                    <meta http-equiv="refresh" content="2;url=barang.php" />
+
+                            <?php
+                                                } else {
+                                                    // Tampilkan eror jika ada kesalahan di database
+                                                    echo mysqli_error($conn);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            ?>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Wahana Mulya</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-warning" href="../php/logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/chart-area-demo.js"></script>
+    <script src="../js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
